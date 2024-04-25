06d930f02448ec1fdeed59444c8676efdc9c5765
tamannasuthar <tamannasuthar2312@gmail.com>
2024-04-25 10:07:00 +0530

    Added sum file

--- Day1/sum.java
+++ Day1/sum.java
@@ -1,5 +1,14 @@
 package Day1;
-
+import java.util.*;
 public class sum {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in); 
+        System.out.println("Enter first number : ");
+        int a = sc.nextInt();
+        System.out.println("Enter second number : ");
+        int b = sc.nextInt();
+        int sum = a + b;
+        System.out.println(" The sum of the number is : " + sum);
+        sc.close();
+    }
 }
