7951720bc8302db8e062754de57f62e7bc2551cd
tamannasuthar <tamannasuthar2312@gmail.com>
2024-04-25 09:54:56 +0530

    Added product file

---
+++ Day2/product.java
@@ -0,0 +1,15 @@
+package Day2;
+import java.util.*;
+public class product {
+    public static void main(String[] args) {
+        System.out.println("Ready");
+        Scanner sc = new Scanner(System.in);
+        System.out.print("enter first number : " );
+        int a = sc.nextInt();
+        System.out.print("enter second number : " );
+        int b = sc.nextInt();
+        int product = a * b;
+        System.out.println("the product of number is : " + product); 
+        sc.close();
+    }
+}
