8d7db635d7e6e694fd652aa1f9de61849b2f0222
tamannasuthar <tamannasuthar2312@gmail.com>
2024-04-26 16:05:45 +0530

    Edited condition file

--- Day1/conditions.java
+++ Day1/conditions.java
@@ -27,12 +27,12 @@ int b = sc.nextInt();
 
 if(a == b) {
     System.out.println("Equal");
-} else {
-    if(a > b) {
+} else  if(a > b) {
         System.out.println("a is greater ");
-    } else {
+    } 
+    else {
         System.out.println("a is lesser ");
     }
-}
+
     }
 }
