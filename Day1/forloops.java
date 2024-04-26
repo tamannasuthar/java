552d112fc3963777f432f8864bd30ac6e8b24537
tamannasuthar <tamannasuthar2312@gmail.com>
2024-04-26 18:30:12 +0530

    Added forloops

---
+++ Day1/forloops.java
@@ -0,0 +1,16 @@
+public class forloops {
+    
+    public static void main(String[] args) {
+//         for(int counter = 0; counter < 100; counter = counter + 1) {
+// System.out.println("Hello World");
+//         }        
+
+// for(int counter = 0; counter < 11; counter++) {
+//     System.out.println(counter+" ");
+// }
+
+for(int i = 0; i < 11; i++) {
+    System.out.println(i);
+}
+    }
+}
