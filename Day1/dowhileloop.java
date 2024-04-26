668d467472c511fa80a36bd7de895ceead49865a
tamannasuthar <tamannasuthar2312@gmail.com>
2024-04-26 18:54:20 +0530

    Added dowhileloop

---
+++ Day1/dowhileloop.java
@@ -0,0 +1,19 @@
+public class dowhileloop {
+    public static void main(String[] args) {
+        // int i = 0;
+        // do {
+        //     System.out.println(i);
+        //     i = i + 1;
+        // } while(i < 11); 
+
+        int i = 12;
+        while(i < 11) {
+            System.out.println("Tamanna suthar");
+        }
+
+        do {
+            System.out.println("Tamanna suthar");
+        } while(i < 11);
+    }
+    
+}
