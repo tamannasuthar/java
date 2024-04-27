992d014a5cbb49e7e96ec00a01a46abf2a85bf91
tamannasuthar <tamannasuthar2312@gmail.com>
2024-04-27 15:40:20 +0530

    Added SI file

---
+++ Day2/simpleinterest.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class simpleinterest {
+    public static void main(String[] args) {
+        Scanner sc =  new Scanner(System.in);
+        System.out.print("Enter principle(P) = ");
+        int P = sc.nextInt();
+        System.out.print("Enter R = ");
+        int R = sc.nextInt();
+        System.out.print("Enter N = ");
+        int N = sc.nextInt();
+        int simpleInterest =(P * R * N ) / 100;
+        System.out.println("Simpleinterest = " + simpleInterest );
+    }
+}
