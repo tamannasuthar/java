986fbd021a33e203d8d5b3aa0a6fe8d057723123
tamannasuthar <tamannasuthar2312@gmail.com>
2024-04-27 15:14:08 +0530

    Edited file

--- Day2/product.java
+++ Day2/product.java
@@ -1,15 +1,23 @@
-package Day2;
+
 import java.util.*;
 public class product {
     public static void main(String[] args) {
-        System.out.println("Ready");
-        Scanner sc = new Scanner(System.in);
-        System.out.print("enter first number : " );
-        int a = sc.nextInt();
-        System.out.print("enter second number : " );
-        int b = sc.nextInt();
-        int product = a * b;
-        System.out.println("the product of number is : " + product); 
-        sc.close();
+        // System.out.println("Ready");
+        // Scanner sc = new Scanner(System.in);
+        // System.out.print("enter first number : " );
+        // int a = sc.nextInt();
+        // System.out.print("enter second number : " );
+        // int b = sc.nextInt();
+        // int product = a * b;
+        // System.out.println("the product of number is : " + product); 
+        // sc.close();
+ Scanner sc = new Scanner(System.in);
+ System.out.println("Enter side");
+ int side = sc.nextInt();
+ int product = side * side;
+ System.out.println("Area of square = "  + product);
+ sc.close();
+       
+
     }
 }
