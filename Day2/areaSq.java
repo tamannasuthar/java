8a0c1cd9975612d3c62ed1e8546f228eb909807f
tamannasuthar <tamannasuthar2312@gmail.com>
2024-04-27 15:11:16 +0530

    Added area file

---
+++ Day2/areaSq.java
@@ -0,0 +1,15 @@
+
+import java.util.*;
+public class areaSq {
+public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter side 2 ");
+ int side = sc.nextInt();
+ int product = side * side;
+ System.out.println("Area of square = "  + product);
+ sc.close();
+}
+   
+    
+
+}
