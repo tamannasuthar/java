0218892b6f7d988e2e645bbf5b007c3e13cdec33
tamannasuthar <tamannasuthar2312@gmail.com>
2024-05-02 21:47:53 +0530

    added rombus pattern

---
+++ Day3/solidrombus.java
@@ -0,0 +1,29 @@
+public class solidrombus {
+    public static void main(String[] args) {
+        int n = 5;
+        for(int i = 1; i <= n; i++) {
+            for(int j = 1; j <= n-i; j++) {
+                System.out.print(" ");
+            }
+            for(int j = 1; j <= n; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+
+
+        }
+        for(int i = 1; i <= n; i++) {
+        for(int j = 1; j <= n-i; j++) {
+           System.out.print(" ");
+          }
+          for(int j = 1; j <= n; j++) {
+            if(i == 1 || j == 1 || i == n || j == n) {
+                System.out.print("*");
+            } else {
+                System.out.print(" ");
+            }
+             }
+             System.out.println();
+        }
+    }
+}
