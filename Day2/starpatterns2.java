13d0d88db04fd588033c42c15fbaf4a76864fc16
tamannasuthar <tamannasuthar2312@gmail.com>
2024-05-05 23:56:11 +0530

    Edited Pattern

--- Day2/starpatterns2.java
+++ Day2/starpatterns2.java
@@ -3,14 +3,15 @@ import java.util.*;
 public class starpatterns2 {
     public static void main(String[] args) {
        Scanner sc = new Scanner(System.in);
-       int n = 4;
-       int m = 5;
-for(int i = 1; i<=n; i++) {
-   for(int j = 1; j<=m; j++) {
-    if(i == 1 || j == 1 || i == n || j == m) {
-        System.out.print("*");
+       System.out.print("Enter side : ");
+       int side = sc.nextInt();
+    //    int m = 5;
+for(int i = 1; i<=side; i++) {
+   for(int j = 1; j<=side; j++) {
+    if(i == 1 || j == 1 || i == side || j == side) {
+        System.out.print(" * ");
     } else {
-        System.out.print(" ");
+        System.out.print("   ");
     }
    }
    System.out.println();
