686e69ddcd2eabbae55f31085b91488c8641ba13
tamannasuthar <tamannasuthar2312@gmail.com>
2024-05-06 14:04:16 +0530

    Added month name file

---
+++ Day2/monthName.java
@@ -0,0 +1,56 @@
+import java.util.*;
+public class monthName {
+    public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the number of the month (1-12): ");
+        int monthNumber = scanner.nextInt();
+
+        String monthName;
+        switch (monthNumber) {
+            case 1:
+                monthName = "January";
+                break;
+            case 2:
+                monthName = "February";
+                break;
+            case 3:
+                monthName = "March";
+                break;
+            case 4:
+                monthName = "April";
+                break;
+            case 5:
+                monthName = "May";
+                break;
+            case 6:
+                monthName = "June";
+                break;
+            case 7:
+                monthName = "July";
+                break;
+            case 8:
+                monthName = "August";
+                break;
+            case 9:
+                monthName = "September";
+                break;
+            case 10:
+                monthName = "October";
+                break;
+            case 11:
+                monthName = "November";
+                break;
+            case 12:
+                monthName = "December";
+                break;
+            default:
+                monthName = "Invalid month number";
+        }
+
+        System.out.println("The month is: " + monthName);
+
+        scanner.close();
+    }
+}
