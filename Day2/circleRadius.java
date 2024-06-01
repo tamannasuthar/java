35f243b912e2c0d259146517c60ebfb4348ac1b6
tamannasuthar <tamannasuthar2312@gmail.com>
2024-06-01 11:46:49 +0530

    Added radius of circle

---
+++ Day2/circleRadius.java
@@ -0,0 +1,31 @@
+// import java.util.*;
+
+// public class circleRadius {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+
+//         // get the area of the circle from the user
+//         System.out.print("Enter the area of the circle : ");
+//     }
+    
+// }
+
+
+import java.util.Scanner;
+
+public class circleRadius {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Get the area of the circle from the user.
+        System.out.print("Enter the area of the circle: ");
+        double area = sc.nextDouble();
+
+        // Calculate the radius of the circle.
+        double radius = Math.sqrt(area / Math.PI);
+
+        // Print the radius of the circle.
+        System.out.println("The radius of the circle is: " + radius);
+    }
+}
